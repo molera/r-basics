cc039687925e686ec48fdaa4ba53d2e1f11ad3c9
Patrick Kraft <kraft.pw@gmail.com>
2025-01-21 22:59:50 +0100

    add files for first session

---
+++ lab/lab01-template.R
@@ -0,0 +1,128 @@
+# ========================================================================= #
+# R Session 1: Introduction to base R & RStudio
+# Author: 
+# Date: 
+# ========================================================================= #
+
+
+
+# Basic arithmetic operations ---------------------------------------------
+
+
+
+# Objects in R ------------------------------------------------------------
+
+
+
+## Vectors ----
+
+
+### Exercise 1 ###
+
+# Every time a section begins with "### Exercise 1 ###"
+# there is a small problem that you should solve
+
+# Generate three objects:
+# a) a should contain the number pi (try out command pi)
+# b) b should contain the numbers 1 to 10
+# c) c should contain the multiplication of 1*pi, 2*pi, ..., 10*pi
+
+
+
+## Matrices ----
+
+
+### Exercise 2 ### 
+
+# combine pi and the vector b in a matrix called m2 (column wise)
+# calculate pi * b and add it to your matrix as an additional column
+# Note: try to solve this task by only creating one object (m2).
+# Call the result of 4*pi from the matrix
+
+
+
+## Data frames ----
+
+
+### Exercise 3 ###
+
+# Select observations from df
+# a) where y is greater than 20
+# b) where x is greater than 1 and y is smaller or equal than 40
+
+
+
+## Lists ----
+
+
+
+## Functions ----
+
+
+
+# Working with data frames ------------------------------------------------
+
+
+
+## Summarizing data ----
+
+
+
+## Missing values ----
+
+
+
+## Recoding data ----
+
+
+### Exercise 4 ###
+
+# a) recode the 'survived' variable in the titanic dataset
+#    to a new numeric dummy, where 1 indicates survivors and 0 non-survivors
+# b) recode the 'class' variable in the titanic dataset
+#    to a new dichotomous factor variable indicating "passenger" and "crew"
+# c) generate a variable in students indicating the number of different 
+#    softwares each of you have learned previously
+
+
+
+## Exporting datasets ----
+
+
+
+# Basic Plotting Functions ------------------------------------------------
+
+
+
+## Scatterplot ----
+
+
+
+## Bar Plot ----
+
+
+
+## Histogram ----
+
+
+
+## Density plots ----
+
+
+
+## Exporting figures ----
+
+
+
+# Packages & Getting Help -------------------------------------------------
+
+
+
+## Example: Creating crosstabs with percentages ----
+
+
+
+
+
+
+
